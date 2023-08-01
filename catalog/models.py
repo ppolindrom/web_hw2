cedf59e08ab018cbea276696605dff19cea63cd8
ppolindrom <ipollyce@mail.ru>
2023-08-01 16:35:01 +0300

    add new change

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=150, verbose_name='наименование')
+    text = models.TextField(max_length=10000, verbose_name='описание')
+    image = models.ImageField(verbose_name='изображение')
+    category = models.CharField(verbose_name='категория')
+    price = models.IntegerField(verbose_name='цена')
+    date_creation = models.DateTimeField(verbose_name='дата создания')
+    date_change = models.DateTimeField(verbose_name='дата изменений')
+
+    # last_name = models.CharField(max_length=150, verbose_name='фамилия')
+    #
+    # def __str__(self):
+    #     # Строковое отображение объекта
+    #     return f'{self.first_name} {self.last_name}'
+    #
+    # class Meta:
+    #     verbose_name = 'студент' # Настройка для наименования одного объекта
+    #     verbose_name_plural = 'студенты'
