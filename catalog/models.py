0a504d8aa36c81728874fa87afb569a1b2698a6c
ppolindrom <ipollyce@mail.ru>
2023-08-02 15:37:30 +0300

    add new change

--- catalog/models.py
+++ catalog/models.py
@@ -1,20 +1,27 @@
 from django.db import models
 
+NULLABLE = {'blank': True, 'null': True}  # константа для необязательного поля
+
+
 class Product(models.Model):
     title = models.CharField(max_length=150, verbose_name='наименование')
     text = models.TextField(max_length=10000, verbose_name='описание')
-    image = models.ImageField(verbose_name='изображение')
+    image = models.ImageField(verbose_name='изображение', **NULLABLE)
     category = models.CharField(verbose_name='категория')
     price = models.IntegerField(verbose_name='цена')
     date_creation = models.DateTimeField(verbose_name='дата создания')
     date_change = models.DateTimeField(verbose_name='дата изменений')
 
-    # last_name = models.CharField(max_length=150, verbose_name='фамилия')
-    #
-    # def __str__(self):
-    #     # Строковое отображение объекта
-    #     return f'{self.first_name} {self.last_name}'
-    #
-    # class Meta:
-    #     verbose_name = 'студент' # Настройка для наименования одного объекта
-    #     verbose_name_plural = 'студенты'
+    def __str__(self):
+        # Строковое отображение объекта
+        return f'{self.title}. {self.text}'
+
+    class Meta:
+        verbose_name = 'кетегория'  # Настройка для наименования одного объекта
+        verbose_name_plural = 'кадегории'
+        ordering = ('title',)
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, verbose_name='наименование')
+    text = models.TextField(max_length=10000, verbose_name='описание')
+    blabla
