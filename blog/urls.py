8cc8cbab8df384cef5a8cce00bebd0be374e111b
ppolindrom <ipollyce@mail.ru>
2023-08-09 16:31:31 +0300

    add new change

--- blog/urls.py
+++ blog/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 
 from blog.apps import BlogConfig
 from blog.views import BlogCreateView, BlogListView, BlogDetailView, BlogUpdateView, BlogDelete
-from catalog.views import blog_list_view
+
 
 app_name = BlogConfig.name
 
 urlpatterns = [
     path('create/', BlogCreateView.as_view(), name='create'),
+    path('', BlogListView.as_view(), name='list'),
     path('view/<int:pk>/', BlogDetailView.as_view(), name='view'),
     path('edit/<int:pk>/', BlogUpdateView.as_view(), name='edit'),
     path('delete/<int:pk>/', BlogDelete.as_view(), name='delete'),
-    path('list/', blog_list_view, name='list'),
 ]
